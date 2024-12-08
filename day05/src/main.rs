da4f8b83fb731c8ecda8e5527aca5181bf88828c
kovapatrik <kovapatrik@gmail.com>
2024-12-08 22:59:50 +0100

    feat: day05, part2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,112 +1,38 @@
-use std::{collections::HashMap, ops::Index};
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = std::fs::read_to_string("day05/src/input.txt").unwrap();
     let manual = parse_input(&input);
-    println!("Part 1: {}", part1(manual));
+
+    let (part1, part2) = solution(manual);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
-fn create_update_from_rules(
-    rules: HashMap<usize, Vec<usize>>,
-    numbers_of_page: Vec<usize>,
-) -> Vec<usize> {
-    let mut page_update = Vec::new();
-    for num in numbers_of_page.iter() {
-        println!("{:?} {:?}", num, page_update);
-        match rules.get(num) {
-            Some(pages_before) => {
-                let valid_pages_before: Vec<usize> = pages_before
-                    .iter()
-                    .cloned()
-                    .filter(|page| numbers_of_page.contains(page))
-                    .collect();
-                println!("{:?}", valid_pages_before);
-                page_update = page_update
-                    .iter()
-                    .cloned()
-                    .filter(|page| !valid_pages_before.contains(page))
-                    .collect();
-                page_update.extend(valid_pages_before);
-            }
-            None => (),
+fn solution(manual: Manual) -> (usize, usize) {
+    let mut part1 = 0;
+    let mut part2 = 0;
+    manual.page_updates.iter().cloned().for_each(|update| {
+        if update.is_sorted_by(|a, b| manual.rules.get(b).map_or(false, |set| set.contains(a))) {
+            part1 += update[update.len() / 2];
+        } else {
+            let mut sorted_update = update.clone();
+            sorted_update.sort_by(|a, b| {
+                manual
+                    .rules
+                    .get(b)
+                    .map_or(false, |set| set.contains(a))
+                    .cmp(&true)
+            });
+            part2 += sorted_update[sorted_update.len() / 2];
         }
-        page_update.push(*num);
-    }
-    page_update
-}
-
-fn is_valid_page_update(rules: HashMap<usize, Vec<usize>>, update: Vec<usize>) -> bool {
-    for (i, page_number) in update.iter().cloned().enumerate() {
-        match rules.get(&page_number) {
-            Some(pages_before) => {
-                if update[..i]
-                    .iter()
-                    .any(|page_before| !pages_before.contains(page_before))
-                    || update[i + 1..]
-                        .iter()
-                        .any(|page_before| pages_before.contains(page_before))
-                {
-                    return false;
-                }
-            }
-            None => {
-                continue;
-            }
-        }
-    }
-    true
-}
-
-fn part1(manual: Manual) -> usize {
-    manual
-        .page_updates
-        .iter()
-        .cloned()
-        .filter(|update| is_valid_page_update(manual.rules.clone(), update.clone()))
-        .map(|valid_update| valid_update[valid_update.len() / 2])
-        .sum()
-}
-
-fn part2(manual: Manual) -> usize {
-    manual
-        .page_updates
-        .iter()
-        .cloned()
-        .filter(|update| !is_valid_page_update(manual.rules.clone(), update.clone()))
-        .map(|invalid_update| {
-            let mut fixed_update = Vec::new();
-
-            for i in 0..fixed_update.len() {
-                let page_number = fixed_update[i];
-                match manual.rules.get(&page_number) {
-                    Some(pages_before) => {
-                        pages_before
-                            .iter()
-                            .cloned()
-                            .filter(|page_before| {
-                                !invalid_update[..i].contains(page_before)
-                                    && invalid_update[i + 1..].contains(page_before)
-                            })
-                            .for_each(|page_before| {
-                                fixed_update[i] = page_before;
-                            });
-                    }
-                    None => {
-                        fixed_update.push(page_number);
-                    }
-                }
-            }
-
-            println!("{:?} -> {:?}", invalid_update, fixed_update);
-
-            fixed_update[fixed_update.len() / 2]
-        })
-        .sum()
+    });
+    (part1, part2)
 }
 
 #[derive(Debug, Clone)]
 struct Manual {
-    rules: HashMap<usize, Vec<usize>>,
+    rules: HashMap<usize, HashSet<usize>>,
     page_updates: Vec<Vec<usize>>,
 }
 
@@ -119,8 +45,8 @@ fn parse_input(input: &str) -> Manual {
         let page_before: usize = page_before.parse().unwrap();
         let page: usize = page.parse().unwrap();
 
-        let entry = rules_map.entry(page).or_insert(Vec::new());
-        entry.push(page_before);
+        let entry = rules_map.entry(page).or_insert(HashSet::new());
+        entry.insert(page_before);
     }
 
     let page_updates: Vec<Vec<usize>> = page_updates
@@ -173,20 +99,6 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(parse_input(TEST_INPUT)), 143);
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(parse_input(TEST_INPUT)), 123);
-    }
-
-    #[test]
-    fn test_create_update_from_rules() {
-        let manual = parse_input(TEST_INPUT);
-        assert_eq!(
-            create_update_from_rules(manual.rules.clone(), manual.page_updates[0].clone()),
-            vec![47, 53]
-        );
+        assert_eq!(solution(parse_input(TEST_INPUT)), (143, 123));
     }
 }
